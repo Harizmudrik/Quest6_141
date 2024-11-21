077dc555c3bbb31f2d36bc12dc50de4a501cce90
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-11-21 21:36:37 +0700

    penambahan uiState: Objek Mahasiswa yang menyimpan data seperti nim, nama, dan email

---
+++ app/src/main/java/com/example/prak6pam/ui/view/screen/MatakuliahView.kt
@@ -0,0 +1,33 @@
+package com.example.prak6pam.ui.view.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.prak6pam.R
+import com.example.prak6pam.ui.model.Mahasiswa
+import com.example.prak6pam.ui.model.Matakuliah
+import com.example.prak6pam.ui.view.screen.part.NavUniv
+
+@Composable
+fun MatakuliahView(
+    uiState: Mahasiswa,
