b7654021583c34cd19e273f4215ac0754cc28a23
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-11-21 21:52:55 +0700

    membuat TampilView untuk menampilkan hasil akhir dan uiState: Objek Mahasiswa yang menyimpan data seperti nim, nama, email, namaMatakuliah, dan kelas

---
+++ app/src/main/java/com/example/prak6pam/ui/view/screen/TampilView.kt
@@ -0,0 +1,31 @@
+package com.example.prak6pam.ui.view.screen
+
+import android.widget.Space
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import com.example.prak6pam.R
+import com.example.prak6pam.ui.model.Mahasiswa
+import com.example.prak6pam.ui.view.screen.part.NavUniv
+
+Composable
+fun TampilView(
+    uiState: Mahasiswa,
+
+) {
+
+}
